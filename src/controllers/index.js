7f75289ce5f946055cc8daa9ca02c5fa38b02af1
Gihwan Kim <ghkim3221@gmail.com>
2018-09-26 23:04:06 +0900

    Implement getting a thing with thingId

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.use('/category', require('./category'));
 router.use('/search', require('./search'));
+router.use('/thing', require('./thing'));
 
 module.exports = router;
